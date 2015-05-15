d76dd29e40184f6644996691c8b573d59aa0433d
hideaki nemoto <hideaki.nemoto@nubee.com>
2015-05-15 16:59:59 +0900

    Added organization/project related routings.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,34 @@ Rails.application.routes.draw do
   get 'home/show'
   root 'home#index'
 
-  resources :user, only: [:edit, :update]
+  resource :user, only: [:edit, :update] do
+    resources :organizations, only: :index, controller: 'user_organizations'
+    resources :projects, only: :index, controller: 'user_projects'
+  end
+
+  resources :organizations, only: [:new, :create, :show] do
+    member do
+      get :dashboard
+      post :contact
+    end
+
+    resources :members, only: [:create, :destroy, :update], controller: 'organization_members' do
+      member do
+        patch :revoke
+      end
+    end
+
+    collection do
+      get :search
+    end
+
+    resources :projects, only: [:new, :create, :show] do
+      resources :members, only: [:create, :destroy, :update], controller: 'project_members' do
+        member do
+          patch :revoke
+        end
+      end
+    end
+  end
+
 end
