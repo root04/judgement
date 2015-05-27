4aa637953fa8cb411db04bf298769449e1dfa520
hideaki nemoto <hideaki.nemoto@nubee.com>
2015-05-27 17:50:17 +0900

    Added relations for project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,10 +4,14 @@ class Project < ActiveRecord::Base
   has_many :user_projects
   has_many :users, through: :user_projects
 
+  has_many :budget
+  has_one :actual
+
   validates :name, presence: true
   validates :organization_id, presence: true
 
   accepts_nested_attributes_for :user_projects
+  accepts_nested_attributes_for :actual
 
   def dismiss_member(user)
     user_projects.member(user).each(&:destroy)
